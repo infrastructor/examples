8c6db2d0d33a1c0e12f0bbb41322b4ea365afc98
Jorge Viana <secoviana@gmail.com>
2019-07-12 16:08:06 +0200

    fixing elastic search example

--- elasticsearch-deployment/provision.groovy
+++ elasticsearch-deployment/provision.groovy
@@ -1,10 +1,10 @@
 def keypath(def machine) { ".vagrant/machines/$machine/virtualbox/private_key" }
 
 inlineInventory {
-  node id: 'elastic-1', host: '192.168.55.11', username: 'ubuntu', keyfile: keypath('elastic-1')
-  node id: 'elastic-2', host: '192.168.55.12', username: 'ubuntu', keyfile: keypath('elastic-2')
-  node id: 'elastic-3', host: '192.168.55.13', username: 'ubuntu', keyfile: keypath('elastic-3')
-  node id: 'elastic-4', host: '192.168.55.14', username: 'ubuntu', keyfile: keypath('elastic-4')
+  node id: 'elastic-1', host: '192.168.55.11', username: 'vagrant', keyfile: keypath('elastic-1')
+  node id: 'elastic-2', host: '192.168.55.12', username: 'vagrant', keyfile: keypath('elastic-2')
+  node id: 'elastic-3', host: '192.168.55.13', username: 'vagrant', keyfile: keypath('elastic-3')
+  node id: 'elastic-4', host: '192.168.55.14', username: 'vagrant', keyfile: keypath('elastic-4')
 }.provision {
 
     task name: "install docker", parallel: 4, actions: {
@@ -17,12 +17,12 @@ inlineInventory {
     }
 
     task name: "run elasticsearch nodes", actions: {
-      shell """
+      shell user: 'root', command: """
         docker run -d -p 9200:9200 -p 9300:9300 \
         -e "node.name=${node.id}" \
         -e "node.master=true" \
         -e "network.publish_host=${node.host}" \
-        -e "discovery.zen.ping.unicast.hosts=${nodes*.host.join(',')}" \
+        -e "discovery.zen.ping.unicast.hosts=${inventory.nodes*.value.host.join(',')}" \
         -e "ES_JAVA_OPTS=-Xmx1024m -Xms1024m" \
         docker.elastic.co/elasticsearch/elasticsearch:5.6.3
       """
